10c6f87fcf495306976b26d2d98c0e65c5da6753
Daleffe <augustodaleffe@gmail.com>
2025-04-28 15:51:02 -0300

    algoritmo jantar dos filósofos

---
+++ Problema do Jantar dos filosofos (avaliativo) 2/main.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+volatile char filosofos[5] = {'P','P','P','P','P'};
+
+sem_t semaforos[5];
+
+pthread_mutex_t mutex;
+
+void* f_mutex(void* arg){
+    pthread_mutex_lock(&mutex);
+    int i = (int) arg;
+
+    if(filosofos[i]== 'P'){
+        filosofos[i] = 'F';
+        sem_wait(&semaforos[i]);
+    
+    } else if (filosofos[i] == 'F' && filosofos[(i+4)%5] != 'C' && filosofos[(i+1)%5] != 'C'){
+        filosofos[i] = 'C';
+        sem_post(&semaforos[i]);
+
+    } else {
+        filosofos[i] = 'P';
+
+    }
+
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+
+void* f_filosofos(void* arg){
+    
+    while(1){
+        int i = (int) arg;
+        int sleep = rand() % 10000;
+        usleep(sleep);
+
+        f_mutex((void*) i);
+    }
+
+
+}
+
+void* f_print(void* arg){
+    while(1){
+        for(int i = 0; i < 5; i++){
+            printf("%c ", filosofos[i]);
+        }
+        printf("\n");
+        usleep(8000);
+    }
+}
+
+int main(){
+
+    pthread_t t1, t2, t3, t4, t5, t6;
+
+    pthread_mutex_init(&mutex, NULL);
+
+    for(int i = 0; i < 5; i++){
+        sem_init(&semaforos[i], 0, 0);
+    }
+
+    pthread_create(&t1, NULL, f_filosofos, (void*) 0);
+    pthread_create(&t2, NULL, f_filosofos, (void*) 1);
+    pthread_create(&t3, NULL, f_filosofos, (void*) 2);
+    pthread_create(&t4, NULL, f_filosofos, (void*) 3);
+    pthread_create(&t5, NULL, f_filosofos, (void*) 4);
+    pthread_create(&t6, NULL, f_print, NULL);
+
+    pthread_join(t1,NULL);
+    pthread_join(t2,NULL);
+    pthread_join(t3,NULL);
+    pthread_join(t4,NULL);
+    pthread_join(t5,NULL);
+    pthread_join(t6,NULL);
+
+
+    for(int i = 0; i < 5; i++){
+        sem_destroy(&semaforos[i]);
+    }
+
+    pthread_mutex_destroy(&mutex);
+
+    return 0;
+
+
+}
+
+
+
+
+// pthread_t t1, t2, t3, t4, t5;
+
+// pthread_mutex_t mutex;
+
+// sem_t garfos[5];
+
+// void* f_mutex(void* arg);
+// void* f_garfos(void* arg);
+
+// int main(){
+
+
+//     return 0;
+// }
+
+// void* f_mutex(void* arg){
+//     pthread_mutex_lock(&mutex);
+//     int i = (int) arg;
+
+//     if(filosofos[i]== 'P'){
+//         filosofos[i] == 'F';
+    
+//     } else if (filosofos[i] == 'F'){
+//         filosofos[i] == 'C';
+//     }
+//     pthread_mutex_unlock(&mutex);
+// }
+
+// void* f_garfos(void* arg){
+//     int i = (int) arg;
+//     int sleep = rand() % 1000;
+//     sleep_for(sleep);
+    
+//     if(filosofos[i] == 'P'){
+//         sem_wait(&garfos[i]);
+//         sem_wait(&garfos[(i+1)%5]);
+//         filosofos[i] = 'F';
+//     } else if (filosofos[i] == 'F'){
+//         sem_post(&garfos[i]);
+//         sem_post(&garfos[(i+1)%5]);
+//         filosofos[i] = 'C';
+//     }
+
+// }
